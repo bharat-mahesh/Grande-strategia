6af7fdb50de3092e1833ef382903576dd502b5c9
Bharat <mbharat04@gmail.com>
2023-04-06 13:26:09 +0530

    throttle and brake plots

--- plot.py
+++ plot.py
@@ -14,19 +14,34 @@ shard_iterator = client.get_shard_iterator(
 )['ShardIterator']
 
 # initialize the plot
-fig, ax = plt.subplots()
+fig, (ax,ax_throttle,ax_brake) = plt.subplots(nrows=3, ncols=1, sharex=True)
 ax.set_xlabel('Distance')
 ax.set_ylabel('Speed')
-ax.set_title('Real-time Speed Data')
+
+
+ax_throttle.set_xlabel('Distance')
+ax_throttle.set_ylabel('Throttle')
+
+
+ax_brake.set_xlabel('Distance')
+ax_brake.set_ylabel('Brake')
 
 # set the x and y limits of the plot
 ax.set_xlim(0, 5412)
 ax.set_ylim(0, 400)
 
+ax_throttle.set_xlim(0, 5412)
+ax_throttle.set_ylim(0, 2)
+
+ax_brake.set_xlim(0, 5412)
+ax_brake.set_ylim(0, 2)
+
 # initialize the line object
 line, = ax.plot([], [])
-
+line2,=ax_throttle.plot([],[])
+line3,=ax_brake.plot([],[])
 # initialize the list of speeds and dictionary of lap counts
+throttle=[]
 speeds = []
 distance=[]
 count=0
@@ -53,9 +68,13 @@ while True:
             data_str=record["Data"].decode("utf8")
             data_obj = json.loads(data_str)
             m_speed = data_obj['m_speed']
+            m_throttle=data_obj['m_throttle']
+            m_brake=data_obj['m_brake']
             if m_speed !=0:
                 speeds.append(m_speed)
                 distance.append(m_lapdistance)                
+                throttle.append(m_throttle)
+                brake.append(m_brake)
         # update the dictionary of lap counts
     # print("SPEED",speeds)
     # print("Distance",distance)    
@@ -64,11 +83,19 @@ while True:
     if lap_counts !=m_lapNumber:
         speeds=[]
         distance=[]
+        throttle=[]
+        brake=[]
         lap_counts=m_lapNumber
     
     # update the plot data
     line.set_xdata(distance)
     line.set_ydata(speeds)
+
+    line2.set_xdata(distance)
+    line2.set_ydata(throttle)
+
+    line3.set_xdata(distance)
+    line3.set_ydata(brake)
 
     # # update the plot
     plt.draw()
